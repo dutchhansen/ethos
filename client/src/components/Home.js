fe4686fbe146f202028cbbf92b75cfc84c95134a
Dutch Hansen <dutchhansen28@gmail.com>
2022-06-06 20:15:35 -0500

    lorem ipsum

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -54,56 +54,8 @@ function Home(props) {
 
                     <div className='lower-media'>
                         <p>
-                            [Intro]
-                            Aight, yeah, yeah
-
-                            [Verse 1]
-                            The way you suck a dick, you get a B+ (You got it, ho)
-                            The way you suck a dick, you get a Prius (You got it, ho)
-                            You runnin' to the dope, they need to keep up (You's runnin', ho)
-                            That bitch can't keep no balance on that dick, but she a Libra
-                            That bitch can't get a dollar from this shit, it's Ebenezer
-                            I busted on her features and gave her back to Jesus
-                            'Cause if I try to keep it (Yo, my baby mama would be beat your ass, bitch)
-                            She know I got issue, Tisha, Kiesha
-                            You know I got season ticket pleasers
-                            You know I got niggas sittin' in my whip lookin' at this shit like I won't buss
-                            All my dogs got it filled up, it's still crunk, ayy
-                            [Bridge]
-                            All my dogs got it filled up, it's real crunk (Yeah)
-                            All my dogs got it filled up, it's still jumpin'
-                            Just imagine how niggas came up from the South
-                            Knowin' them streets donе did you dirt
-                            Comin' up, these niggas wasn't no fans
-                            Pussy-ass nigga, don't touch my stash
-
-                            [Break]
-                            You got it (Yeah, yеah, yeah)
-                            Top ain't gonna like that, but I like this, check me out
-                            Aight, this my part right here
-
-                            [Verse 2]
-                            Look, I'm tryna take all these bitches to work
-                            Workin' the curbs, workin' the curbs
-                            I wanna count all the Benjamins
-                            She wanna fuck on the Benjamins
-                            Suck on my Benjamins, make a predicament
-                            Breakin' a ligament, have an epiphany
-                            Have her like evidence, have her like every day
-                            We were just kickin' it, we were just kickin' it, ho
-                            I'm tryna take all these bitches to work (Yeah)
-                            Workin' the curbs, workin' the curbs (Man)
-                            I wanna count all the Benjamins (Yeah)
-                            She wanna fuck on the Benjamins (Yeah)
-                            Suck on my Benjamins, make a predicament
-                            Breakin' a ligament, have an epiphany
-                            Have her like evidence, have her like every day
-                            We were just kickin' it, we were just kickin' it, ho (Bitch, haha)
-                            [Outro]
-                            You got it, yeah, yeah
-                            Top ain't gonna like that, but I like this, check me out
-                            Aight, this my part right here
-                            Look—
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         </p>
                     </div>
 
